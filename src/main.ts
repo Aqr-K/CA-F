0dad0ac28e0a11c97721dba15de4b651267b0a61
shenxianmq <31564266@qq.com>
2024-09-04 21:03:55 +0800

    main

--- src/main.ts
+++ src/main.ts
@@ -11,22 +11,6 @@ import "vue3-perfect-scrollbar/style.css";
 import PathList from "./components/input/PathList.vue";
 import PathInput from "./components/input/PathInput.vue";
 
-// 创建 Vuex 实例
-const store = new Vuex.Store({
-  state: {
-    // 在这里定义应用的状态
-  },
-  mutations: {
-    // 在这里定义修改状态的 mutations
-  },
-  actions: {
-    // 在这里定义处理异步操作的 actions
-  },
-  getters: {
-    // 在这里定义计算属性 getters
-  },
-});
-
 const app = createApp(App);
 
 // 注册全局组件
